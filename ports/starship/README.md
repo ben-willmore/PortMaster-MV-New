7efe25dc00be1e3fa2e56e8f60c4311aa9d159df
Ben Willmore <ben@willmore.eu>
2025-03-13 16:19:02 +0000

    Add Jeod starship port

---
+++ ports/starship/README.md
@@ -0,0 +1,27 @@
+## Information
+Starship was built from the `v1.0.0` tag on 12/23/2024. You can build your own binaries by following the [BUILDING.md guide](starship/BUILDING.md).
+
+## Installation
+The supported ROM is the USA 1.1 Rev A version. You can verify you have dumped a supported copy of the game by using the SHA-1 File Checksum Online at https://www.romhacking.net/hash/. The hash for a US 1.1 ROM is SHA-1: 09F0D105F476B00EFA5303A3EBC42E60A7753B7A.
+
+Legally obtain your rom and place it in `ports/starship`, then start the port. Texture pack files can be added to the `ports/starship/mods` folder.
+
+Logs are recorded automatically as `ports/starship/log.txt`. Please provide a log if you report an issue. PortMaster does not maintain the Starship repository and is not responsible for bugs or issues outside of our control. Likewise, HarbourMasters is not affiliated with PortMaster and this distribution is not officially supported by them. *Please come to PortMaster for help before approaching the HarbourMasters!*
+
+## Menu Navigation
+There is a `starship.gptk` file you can use to change which button emulates F1 (default is L3). Some devices have a special button called `guide` that makes for a good F1 mapping.
+
+## Adding Mods
+Mods are available at the port's [Gamebanana Page](https://gamebanana.com/games/21612).
+
+## Default Gameplay Controls
+The port uses SDL controller mapping and controls can be remapped from the menu bar.
+
+## Thanks
+- Nintendo for the game  
+- HarbourMasters for the native pc port  
+- Testers and Devs from the PortMaster Discord  
+
+
+
+
---
+++ ports/starship/Starship.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Set variables
+GAMEDIR="/$directory/ports/starship"
+
+# Exports
+export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG=$sdl_controllerconfig
+export PATCHER_FILE="$GAMEDIR/tools/otrgen"
+export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
+export PATCHER_TIME="5 to 10 minutes"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Check imgui.ini and modify if needed
+input_file="imgui.ini"
+temp_file="imgui_temp.ini"
+skip_section=0
+# Loop through each line in the input file
+while IFS= read -r line; do
+    # Check if the line is a window header
+    if [[ "$line" =~ ^\[Window\]\[Main\ Game\] || "$line" =~ ^\[Window\]\[Main\ -\ Deck\] ]]; then
+        skip_section=1  # Set the flag to skip modifications for this section
+    elif [[ "$line" =~ ^\[Window\] ]]; then
+        skip_section=0  # Reset the flag for other windows
+    fi
+
+    # Modify Pos and Size only if the current section is not skipped
+    if [[ $skip_section -eq 0 ]]; then
+        if [[ "$line" =~ ^Pos=.* ]]; then
+            echo "Pos=30,30" >> "$temp_file"
+        elif [[ "$line" =~ ^Size=.* ]]; then
+            echo "Size=400,300" >> "$temp_file"
+        else
+            echo "$line" >> "$temp_file"
+        fi
+    else
+        # If skipping, write the line unchanged
+        echo "$line" >> "$temp_file"
+    fi
+done < "$input_file"
+
+# Replace the original file with the modified one
+mv "$temp_file" "$input_file"
+
+# Check if we need to generate any o2r files
+if [ ! -f "sf64.otr" ]; then
+    # Ensure we have a rom file before attempting to generate o2r
+    if ls *.*64 1> /dev/null 2>&1; then
+        if [ -f "$controlfolder/utils/patcher.txt" ]; then
+            source "$controlfolder/utils/patcher.txt"
+            $ESUDO kill -9 $(pidof gptokeyb)
+        else
+            pm_message "This port requires the latest version of PortMaster."
+        fi
+    else
+        pm_message "Missing ROM files! Can't generate o2r!"
+    fi
+fi
+
+# Check if OTR files were generated
+if [ ! -f "sf64.otr" ]; then
+    echo "No otr found, can't run the game!"
+    exit 1
+fi
+
+# Run the game
+pm_message "Loading, please wait... (might take a while!)"
+$GPTOKEYB "Starship" -c "starship.gptk" & 
+pm_platform_helper "$GAMEDIR/Starship"
+./Starship
+
+# Cleanup
+rm -rf "$GAMEDIR/logs/"
+pm_cleanup
