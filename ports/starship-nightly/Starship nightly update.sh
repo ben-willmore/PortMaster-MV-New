1513a488dba3fbb2c78e8ffc357d8d98b49e7155
Ben Willmore <ben@willmore.eu>
2025-03-17 08:23:32 +0000

    Nightly updates

---
+++ ports/starship-nightly/Starship nightly update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# PORTMASTER: starship-nightly.zip, Starship nightly.sh
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+# Set variables
+GAMEDIR="/$directory/ports/starship-nightly"
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/update-log.txt") 2>&1
+
+$controlfolder/harbourmaster install https://github.com/ben-willmore/PortMaster-MV-New/releases/download/Starship-nightly-latest/starship-nightly.zip
